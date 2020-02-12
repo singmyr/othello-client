c8f5717026d946008e39d003402dff997ae564d3
Mattias Singmyr <mattias@singmyr.io>
2020-02-12 23:01:22 +0100

    Add the initial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<{}> {
+  ws: WebSocket|null = null;
+  componentDidMount = () => {
+    if (!this.ws) {
+      const self = this;
+      this.ws = new WebSocket("ws://localhost:8080/echo");
+      this.ws.binaryType = 'arraybuffer';
+      this.ws.onopen = function() {
+          console.log("OPEN");
+          let buffer = new ArrayBuffer(1);
+          let view = new DataView(buffer);
+          view.setInt8(0, 2 >> 1)
+          self.ws && self.ws.send(buffer);
+      }
+      this.ws.onclose = function() {
+        console.log("CLOSE");
+          self.ws = null;
+      }
+      this.ws.onmessage = function(evt: any) {
+        console.log("RESPONSE: ", evt.data);
+      }
+      this.ws.onerror = function(evt: any) {
+        console.log("ERROR: ", evt.data);
+      }
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
