0c8e850acf3692420a9e515626bfaf1c9bbe2f20
Mattias Singmyr <mattias@grebban.com>
2020-02-19 19:10:36 +0100

    Send a random name for the playerConnected event (1).

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,15 @@ class App extends Component<{}> {
       this.ws = new WebSocket("ws://localhost:8080/echo");
       this.ws.binaryType = 'arraybuffer';
       this.ws.onopen = function() {
-          console.log("OPEN");
-          let buffer = new ArrayBuffer(1);
+          const name = "Player "+Math.floor(Math.random() * 1000000);
+          let buffer = new ArrayBuffer(1 + name.length);
           let view = new DataView(buffer);
-          view.setInt8(0, 2 >> 1)
+          view.setInt8(0, 1)
+
+          for (let i = 0; i < name.length; i++) {
+            view.setUint8((i+1), name.charCodeAt(i));
+          }
+          console.log(view);
           self.ws && self.ws.send(buffer);
       }
       this.ws.onclose = function() {
@@ -20,6 +25,14 @@ class App extends Component<{}> {
           self.ws = null;
       }
       this.ws.onmessage = function(evt: any) {
+        let view = new DataView(evt.data);
+        const event = view.getUint8(0);
+        console.log('event', event);
+        let str = '';
+        for (let i = 1; i < evt.data.byteLength; i++) {
+          str += String.fromCharCode(view.getUint8(i))
+        }
+        console.log(evt.data.byteLength, str)
         console.log("RESPONSE: ", evt.data);
       }
       this.ws.onerror = function(evt: any) {
